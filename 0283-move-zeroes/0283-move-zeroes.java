1c6042f9d8f437b7883a0bd48d8265df31adfd6e
POORNESH PRAHALATHAN <83975683+PoorneshPrahal@users.noreply.github.com>
2024-07-15 11:06:56 +0530

    Time: 3 ms (25.80%), Space: 46.3 MB (15.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,32 +1,40 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int first = 0;
+        int second;
         int flag = 0;
+        
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i] == 0)
             {
-                first = i;
                 flag = 1;
+                first = i;
                 break;
             }
         }
+        
         if(flag == 0)
         {
             return;
         }
-        int second = first + 1;
-        System.out.println(first + " " + second);
-        while(second < nums.length)
+        
+        second = first + 1;
+        
+        for(int i=second;i<nums.length;i++)
         {
-            if(nums[second] != 0)
+            if(nums[second] != 0 && nums[first] == 0)
             {
-                int temp = nums[first];
-                nums[first] = nums[second];
-                nums[second] = temp;
+                int temp = nums[second];
+                nums[second] = nums[first];
+                nums[first] = temp;
                 first++;
+                second++;
             }
-            second++;
+            else if(nums[first] == 0 && nums[second] == 0)
+            {
+                second++;
+            }
         }
         
     }
